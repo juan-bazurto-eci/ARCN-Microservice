c70de5c2c80ed3b5880c85f5d24b0ff79eaf88fa
Miguel Salamanca <miguelsalamanca007@hotmail.com>
2024-03-22 11:44:54 -0500

    Changed Client id from Long to String

--- src/main/java/edu/escuelaing/arcn/microservice/domain/model/Client.java
+++ src/main/java/edu/escuelaing/arcn/microservice/domain/model/Client.java
@@ -2,7 +2,7 @@ package edu.escuelaing.arcn.microservice.domain.model;
 
 
 public class Client {
-    private Long id;
+    private String id;
     private String name;
     private String email;
     private String address;
@@ -15,11 +15,11 @@ public class Client {
         this.paymentDetails=paymentDetails;
     }
 
-    public Long getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(String id) {
         this.id = id;
     }
 
